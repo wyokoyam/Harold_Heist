83e1a394c7542e1fc5fa8f61532a99735f000da4
Victor Bordo <vbordo724@gmail.com>
2015-03-24 23:19:20 -0500

    Erased unnecessary commented out code.

--- core/src/com/Harold_Heist/View/GameScreen.java
+++ core/src/com/Harold_Heist/View/GameScreen.java
@@ -39,77 +39,12 @@ public class GameScreen implements Screen{
 		this.game = game;
 	}
 	
-//	public GameScreen(final Main game) {
-//		this.game = game;
-//		tableIMG = new Texture(Gdx.files.internal("tableImage.png"));
-//		protagIMG = new Texture(Gdx.files.internal("marioImage.png"));
-//		
-//		// create camera
-//		camera = new OrthographicCamera();
-//		camera.setToOrtho(false, 800, 480);
-//		
-//		// create rectangles hard code, design Table Class
-////		table = new Rectangle();
-////		table.x = 300;
-////		table.y = 40;
-////		table.width = 0;
-////		table.height = 0;
-//		
-//		// create protagonist hard code, design Protagonist Class
-//		protag = new Rectangle();
-//		protag.x = 400;
-//		protag.y = 240;
-//		protag.width = 4;
-//		protag.height = 8;
-//	}
-	
-//	@Override
-//	public void create() {
-//
-//
-//	}
-	
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		keyboardControls();
 		renderer.render();
-		
-		// update camera matrices
-//		camera.update();
-		
-//		game.batch.setProjectionMatrix(camera.combined);
-		
-//		ShapeRenderer debugRenderer = new ShapeRenderer();
-//		debugRenderer.setProjectionMatrix(camera.combined);
-//		debugRenderer.begin(ShapeType.Line);
-		
-		// hard-code tables until design Table Class, table.x, table.y, simple for loop
-//		game.batch.begin();
-//		game.batch.draw(new Table(), 100, 300); 
-//		game.batch.draw(tableIMG, 200, 100);
-//		game.batch.draw(tableIMG, 70,  150);
-//		game.batch.draw(tableIMG, 600, 350);
-//		game.batch.end();
-		
-		
-		//render Protagonist
-//		game.batch.begin();
-//		game.batch.draw(protagIMG, protag.x, protag.y);
-//		game.batch.end();
-		
-		//
-//		if(Gdx.input.isKeyPressed(Keys.LEFT)) protag.x -= 200 * Gdx.graphics.getDeltaTime();
-//	    if(Gdx.input.isKeyPressed(Keys.RIGHT)) protag.x += 200 * Gdx.graphics.getDeltaTime();
-//	    if(Gdx.input.isKeyPressed(Keys.DOWN)) protag.y-= 200 * Gdx.graphics.getDeltaTime();
-//	    if(Gdx.input.isKeyPressed(Keys.UP)) protag.y += 200 * Gdx.graphics.getDeltaTime();
-
-	      // protag stays within the screen bounds
-//	     if(protag.x < 0) protag.x = 0;
-//	     if(protag.x > 800 - 64) protag.x = 800 - 64;
-//	     if(protag.y < 0) protag.y = 0;
-//	     if(protag.y > 480 - 64) protag.y = 480 - 64;
 		
 	}
 
