a000695d4aae102001485a8e6d5add68340458f4
tonybach <tonybach@users.noreply.github.com>
2015-04-15 22:31:46 -0500

    minor code refactoring

--- core/src/com/Harold_Heist/View/CafeMacRenderer.java
+++ core/src/com/Harold_Heist/View/CafeMacRenderer.java
@@ -32,16 +32,16 @@ import java.util.ArrayList;
 
 public class CafeMacRenderer {
 
-	private static final float CAMERA_WIDTH = 25f;
+    private static final float CAMERA_WIDTH = 25f;
 
-	private CafeMac cafeMac;
-	private OrthographicCamera cam;
+    private CafeMac cafeMac;
+    private OrthographicCamera cam;
 
-	private SpriteBatch spriteBatch;
+    private SpriteBatch spriteBatch;
     private BitmapFont font;
-	private boolean debug = false;
-	private float width;
-	private float height;
+    private boolean debug = false;
+    private float width;
+    private float height;
     private ShapeRenderer debugRenderer;
     private ShapeRenderer shapeRenderer;
     private int gameScore;
@@ -49,16 +49,17 @@ public class CafeMacRenderer {
     BitmapFont scoreFont;
 
 
-	public void setSize (float w, float h) {
-		this.width = w;
-		this.height = h;
-	}
-	
-	private TiledMap tiledMap;
-	private BatchTiledMapRenderer tiledMapRenderer;
+    public void setSize(float w, float h) {
+        this.width = w;
+        this.height = h;
+    }
+
+    private TiledMap tiledMap;
+    private BatchTiledMapRenderer tiledMapRenderer;
 
     private Protagonist protag;
     private Antagonist antag;
+    private Fruit fruit;
     private Array<Fruit> fruits;
 
     private ArrayList<Shape2D> collisionShapes;
@@ -69,8 +70,8 @@ public class CafeMacRenderer {
         antag = cafeMac.getAntagonist();
         fruits = cafeMac.getFruitArray();
         this.cafeMac = cafeMac;
-		
-		tiledMap = new TmxMapLoader().load("graphics/cafeMacMap.tmx");
+
+        tiledMap = new TmxMapLoader().load("graphics/cafeMacMap.tmx");
 //		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/32f);
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
@@ -85,13 +86,13 @@ public class CafeMacRenderer {
         this.cam = new OrthographicCamera();
         this.cam.setToOrtho(false, width, height);
 
-		this.cam.update();
-		this.debug = debug;
-		spriteBatch = new SpriteBatch();
+        this.cam.update();
+        this.debug = debug;
+        spriteBatch = new SpriteBatch();
         font = new BitmapFont();
 
         gameScore = 0;
-        gameScoreName = "Score: " + gameScore;
+        gameScoreName = "SCORE: " + gameScore;
         scoreFont = new BitmapFont();
 
         debugRenderer = new ShapeRenderer();
@@ -100,52 +101,28 @@ public class CafeMacRenderer {
         MapObjects collisionObjects = objectLayer.getObjects();
         collisionShapes = new ArrayList<Shape2D>();
 
-        for (MapObject obj: collisionObjects) {
+        for (MapObject obj : collisionObjects) {
             if (obj.getClass() == RectangleMapObject.class) {
-                RectangleMapObject rectObj = (RectangleMapObject)obj;
+                RectangleMapObject rectObj = (RectangleMapObject) obj;
                 Rectangle rect = rectObj.getRectangle();
                 collisionShapes.add(rect);
             } else {
-                EllipseMapObject ellipseObj = (EllipseMapObject)obj;
+                EllipseMapObject ellipseObj = (EllipseMapObject) obj;
                 Ellipse ellip = ellipseObj.getEllipse();
                 collisionShapes.add(ellip);
             }
         }
-	}
+    }
 
-	public void render() {
+    public void render() {
 
         this.cam.update();
         tiledMapRenderer.setView(this.cam);
         tiledMapRenderer.render();
-        if (debug) {
-
-            debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-            for (Shape2D shape: collisionShapes) {
-                if (shape.getClass() == Rectangle.class) {
-                    Rectangle rect = (Rectangle) shape;
-                    debugRenderer.setColor(new Color(0, 1, 0, 1));
-                    debugRenderer.rect(rect.getX(), rect.getY(), rect.width, rect.height);
-
-                } else {
-                    Ellipse ellip = (Ellipse) shape;
-                    debugRenderer.setColor(new Color(0, 1, 0, 1));
-                    debugRenderer.rect(ellip.x, ellip.y, ellip.width, ellip.height);
-                }
-
-            }
-            debugRenderer.end();
-        }
-
-		spriteBatch.begin();
-		drawProtag();
-		drawAntag();
-        drawFruits();
-        scoreFont.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-        scoreFont.draw(spriteBatch, gameScoreName, 730, 20);
-        spriteBatch.end();
+        if (debug) drawDebug();
+        draw();
         collisionHandler();
-	}
+    }
 
     private void collisionHandler() {
         for (Shape2D shape : collisionShapes) {
@@ -158,7 +135,16 @@ public class CafeMacRenderer {
         for (Fruit fruit : cafeMac.getFruitArray()) {
             objectCollisionHandler(protag.getPosition(), fruit.getPosition(), false);
         }
+    }
 
+    private void draw() {
+        spriteBatch.begin();
+        drawProtag();
+        drawAntag();
+        drawFruits();
+        scoreFont.setColor(0, 0, 1, 0.8f);
+        scoreFont.draw(spriteBatch, gameScoreName, 710, 20);
+        spriteBatch.end();
     }
 
     private void obstacleCollisionHandler(Vector2 character, Shape2D shape, boolean isProtag) {
@@ -193,26 +179,83 @@ public class CafeMacRenderer {
             shapeHeight = ellip.height;
         }
 
-//            collision left of object
+        // collision left of object
         if (charX + charSize > shapeX && charX + charSize < shapeX + 10 && charY + charSize > shapeY && charY < shapeY + shapeHeight) {
             character.x = shapeX - charSize;
         }
 
-//            collision right of object
+        // collision right of object
         else if (charX < shapeX + shapeWidth && charX > shapeX + shapeWidth - 10 && charY + charSize > shapeY && charY < shapeY + shapeHeight) {
             character.x = shapeX + shapeWidth;
         }
 
-        //collision top of object
-        else if (charX + charSize> shapeX && charX < shapeX + shapeWidth && charY < shapeY + shapeHeight && charY > shapeY + shapeHeight - 10) {
+        // collision top of object
+        else if (charX + charSize > shapeX && charX < shapeX + shapeWidth && charY < shapeY + shapeHeight && charY > shapeY + shapeHeight - 10) {
             character.y = shapeY + shapeHeight;
         }
 
-        //collision below object
+        // collision below object
         else if (charX + charSize > shapeX && charX < shapeX + shapeWidth && charY + charSize > shapeY && charY + charSize < shapeY + 10) {
             character.y = shapeY - charSize;
         }
     }
+
+    /**
+     * If protagAndAntag is true, handle collision between protagonist and antagonist. If false, handle collision
+     * between protagonist and fruit.
+     */
+    private void objectCollisionHandler(Vector2 object1Position, Vector2 object2Position, boolean protagAndAntag) {
+        float object1X = object1Position.x;
+        float object1Y = object1Position.y;
+        float object1Size = protag.getSize();
+
+        float object2X = object2Position.x;
+        float object2Y = object2Position.y;
+        float object2Size;
+        if (protagAndAntag) object2Size = antag.getSize();
+        else object2Size = fruit.getSize();
+
+        // collision left of object
+        if (object1X + object1Size > object2X && object1X + object1Size < object2X + 10 && object1Y + object1Size > object2Y && object1Y < object2Y + object2Size) {
+            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
+            else {
+                cafeMac.removeFruit(object2Position);
+                gameScore++;
+                gameScoreName = "SCORE: " + gameScore;
+            }
+        }
+
+        // collision right of object
+        else if (object1X < object2X + object2Size && object1X > object2X + object2Size - 10 && object1Y + object1Size > object2Y && object1Y < object2Y + object2Size) {
+            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
+            else {
+                cafeMac.removeFruit(object2Position);
+                gameScore++;
+                gameScoreName = "SCORE: " + gameScore;
+            }
+        }
+
+        // collision top of object
+        else if (object1X + object1Size > object2X && object1X < object2X + object2Size && object1Y < object2Y + object2Size && object1Y > object2Y + object2Size - 10) {
+            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
+            else {
+                cafeMac.removeFruit(object2Position);
+                gameScore++;
+                gameScoreName = "SCORE: " + gameScore;
+            }
+        }
+
+        // collision below object
+        else if (object1X + object1Size > object2X && object1X < object2X + object2Size && object1Y + object1Size > object2Y && object1Y + object1Size < object2Y + 10) {
+            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
+            else {
+                cafeMac.removeFruit(object2Position);
+                gameScore++;
+                gameScoreName = "SCORE: " + gameScore;
+            }
+        }
+    }
+
 
     private void wallCollisionHandler(Vector2 character, boolean isProtag) {
         float charX = character.x;
@@ -231,149 +274,72 @@ public class CafeMacRenderer {
         if (charY + charSize > height) character.y = height - charSize;
     }
 
-
-    /** If protagAndAntag is true, handle collision between protagonist and antagonist. If false, handle collision
-     * between protagonist and fruit.
-     */
-    private void objectCollisionHandler(Vector2 object1Position, Vector2 object2Position, boolean protagAndAntag) {
-        float object1X = object1Position.x;
-        float object1Y = object1Position.y;
-        float object1Size = 32;
-
-        float object2X = object2Position.x;
-        float object2Y = object2Position.y;
-        float object2Size = 32;
-
-        // collision left of object
-        if (object1X + object1Size > object2X && object1X + object1Size < object2X + 10 && object1Y + object1Size > object2Y && object1Y < object2Y + object2Size) {
-            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
-            else {
-                cafeMac.removeFruit(object2Position);
-                gameScore++;
-                gameScoreName = "Score: " + gameScore;
-            }
-        }
-
-        // collision right of object
-        else if (object1X < object2X + object2Size && object1X > object2X + object2Size - 10 && object1Y + object1Size > object2Y && object1Y < object2Y + object2Size) {
-            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
-            else {
-                cafeMac.removeFruit(object2Position);
-                gameScore++;
-                gameScoreName = "Score: " + gameScore;
-            }
-        }
-
-        // collision top of object
-        else if (object1X + object1Size> object2X && object1X < object2X + object2Size && object1Y < object2Y + object2Size && object1Y > object2Y + object2Size - 10) {
-            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
-            else {
-                cafeMac.removeFruit(object2Position);
-                gameScore++;
-                gameScoreName = "Score: " + gameScore;
-            }
-        }
-
-        // collision below object
-        else if (object1X + object1Size > object2X && object1X < object2X + object2Size && object1Y + object1Size > object2Y && object1Y + object1Size < object2Y + 10) {
-            if (protagAndAntag) cafeMac.setState(CafeMac.State.STATE_GAMEOVER);
-            else {
-                cafeMac.removeFruit(object2Position);
-                gameScore++;
-                gameScoreName = "Score: " + gameScore;
-            }
-        }
-    }
-
-	private void drawProtag() {
-		float xCoordinate = protag.getPosition().x ;
-		float yCoordinate = protag.getPosition().y ;
+    private void drawProtag() {
+        float xCoordinate = protag.getPosition().x;
+        float yCoordinate = protag.getPosition().y;
         float protagSize = Protagonist.getSize();
-
-        if (debug) {
-            debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-            Rectangle rect = protag.getBounds();
-            debugRenderer.setColor(new Color(0, 1, 0, 1));
-            debugRenderer.rect(xCoordinate, yCoordinate, rect.width, rect.height);
-            debugRenderer.end();
-        }
 
         if (protag.getState() == Protagonist.State.FACERIGHT) {
             spriteBatch.draw(Assets.protagRight, xCoordinate, yCoordinate, protagSize, protagSize);
+        } else if (protag.getState() == Protagonist.State.FACELEFT) {
+            spriteBatch.draw(Assets.protagLeft, xCoordinate, yCoordinate, protagSize, protagSize);
+        } else if (protag.getState() == Protagonist.State.FACEUP) {
+            spriteBatch.draw(Assets.protagUp, xCoordinate, yCoordinate, protagSize, protagSize);
+        } else if (protag.getState() == Protagonist.State.FACEDOWN) {
+            spriteBatch.draw(Assets.protagDown, xCoordinate, yCoordinate, protagSize, protagSize);
         }
-		else if (protag.getState() == Protagonist.State.FACELEFT) {
-			spriteBatch.draw(Assets.protagLeft, xCoordinate, yCoordinate, protagSize, protagSize);
-		}
-		else if (protag.getState() == Protagonist.State.FACEUP) {
-			spriteBatch.draw(Assets.protagUp, xCoordinate, yCoordinate, protagSize, protagSize);
-		}
-		else if (protag.getState() == Protagonist.State.FACEDOWN) {
-			spriteBatch.draw(Assets.protagDown, xCoordinate, yCoordinate, protagSize, protagSize);
-		}
-	}
-	
-	private void drawAntag(){
-		float xCoordinate = antag.getPosition().x ;
-		float yCoordinate = antag.getPosition().y ;
+    }
+
+    private void drawAntag() {
+        float xCoordinate = antag.getPosition().x;
+        float yCoordinate = antag.getPosition().y;
         float antagSize = Antagonist.getSize();
 
-        if (debug) {
-            debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-            Rectangle rect = antag.getBounds();
-            debugRenderer.setColor(new Color(0, 1, 0, 1));
-            debugRenderer.rect(xCoordinate, yCoordinate, rect.width, rect.height);
-            debugRenderer.end();
-        }
-
         if (antag.getState() == Antagonist.State.FACERIGHT) {
-			spriteBatch.draw(Assets.antagRight, xCoordinate, yCoordinate, antagSize, antagSize);
-		}
-		else if (antag.getState() == Antagonist.State.FACELEFT) {
-			spriteBatch.draw(Assets.antagLeft, xCoordinate, yCoordinate, antagSize, antagSize);
-		}
-		else if (antag.getState() == Antagonist.State.FACEUP) {
-			spriteBatch.draw(Assets.antagUp, xCoordinate, yCoordinate, antagSize, antagSize);
-		}
-		else if (antag.getState() == Antagonist.State.FACEDOWN) {
-			spriteBatch.draw(Assets.antagDown, xCoordinate, yCoordinate, antagSize, antagSize);
-		}
-	}
+            spriteBatch.draw(Assets.antagRight, xCoordinate, yCoordinate, antagSize, antagSize);
+        } else if (antag.getState() == Antagonist.State.FACELEFT) {
+            spriteBatch.draw(Assets.antagLeft, xCoordinate, yCoordinate, antagSize, antagSize);
+        } else if (antag.getState() == Antagonist.State.FACEUP) {
+            spriteBatch.draw(Assets.antagUp, xCoordinate, yCoordinate, antagSize, antagSize);
+        } else if (antag.getState() == Antagonist.State.FACEDOWN) {
+            spriteBatch.draw(Assets.antagDown, xCoordinate, yCoordinate, antagSize, antagSize);
+        }
+    }
 
     private void drawFruits() {
-        for (Fruit fruit: fruits) {
+        for (Fruit fruit : fruits) {
             spriteBatch.draw(Assets.fruitTexture, fruit.getPosition().x, fruit.getPosition().y, fruit.getSize(), fruit.getSize());
         }
     }
-//	private void drawDebug() {
-//        // render blocks
-//        debugRenderer.setProjectionMatrix(cam.combined);
-//        debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-////        for (Table table : cafeMac.getTables()) {
-//        for (Rectangle rect: collisionShapes) {
-////            Rectangle rect = table.getBounds();
-////            float x1 = table.getPosition().x + rect.x;
-////            float y1 = table.getPosition().y + rect.y;
-//            debugRenderer.setColor(new Color(1, 0, 0, 1));
-//            debugRenderer.rect(rect.x, rect.y, rect.width, rect.height);
-//        }
-//    }
-//
-//		// render protagonist
-//		Protagonist protag = cafeMac.getProtagonist();
-//		Rectangle rect = protag.getBounds();
-//		float x1 = protag.getPosition().x + rect.x;
-//		float y1 = protag.getPosition().y + rect.y;
-//		debugRenderer.setColor(new Color(0, 1, 0, 1));
-//		debugRenderer.rect(x1, y1, rect.width, rect.height);
-//		debugRenderer.end();
-//
-//		// render antagonist
-//		Antagonist antag = cafeMac.getAntagonist();
-//		Rectangle Arect = protag.getBounds();
-//		float Ax1 = protag.getPosition().x + rect.x;
-//		float Ay1 = protag.getPosition().y + rect.y;
-//		debugRenderer.setColor(new Color(0, 1, 0, 1));
-//		debugRenderer.rect(Ax1, Ay1, Arect.width, Arect.height);
-//		debugRenderer.end();
-//	}
+
+    private void drawDebug() {
+        debugRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        // Outline the shapes
+        for (Shape2D shape : collisionShapes) {
+            if (shape.getClass() == Rectangle.class) {
+                Rectangle rect = (Rectangle) shape;
+                debugRenderer.setColor(new Color(0, 1, 0, 1));
+                debugRenderer.rect(rect.getX(), rect.getY(), rect.width, rect.height);
+
+            } else {
+                Ellipse ellip = (Ellipse) shape;
+                debugRenderer.setColor(new Color(0, 1, 0, 1));
+                debugRenderer.rect(ellip.x, ellip.y, ellip.width, ellip.height);
+            }
+        }
+
+        // Outline protagonist
+        Rectangle protagBounds = protag.getBounds();
+        debugRenderer.setColor(new Color(0, 1, 0, 1));
+        debugRenderer.rect(protag.getPosition().x, protag.getPosition().y, protagBounds.width, protagBounds.height);
+
+        // Outline antagonist
+        Rectangle antagBounds = antag.getBounds();
+        debugRenderer.setColor(new Color(0, 1, 0, 1));
+        debugRenderer.rect(antag.getPosition().x, antag.getPosition().y, antagBounds.width, antagBounds.height);
+
+        debugRenderer.end();
+    }
+
 }
